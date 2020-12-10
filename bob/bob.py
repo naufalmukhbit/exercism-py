52f9c490ebc7860e5b5933a4a5b6cf7be2685c99
Naufal Mukhbit <nmukhbit@gmail.com>
2020-12-10 12:52:20 +0700

    Finished bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,17 +1,11 @@
 import re
 def response(hey_bob):
-    if not re.search(r'[a-z]',hey_bob):
-        if re.search(r'\?$', hey_bob):
-            return "Calm down, I know what I'm doing!"
-        else:
-            if re.search(r'[A-Z]', hey_bob):
-                return "Whoa, chill out!"
-            else:
-                return "Fine. Be that way!"
+    if re.search(r'\?(?!\s+\w)', hey_bob):
+        return "Calm down, I know what I'm doing!" if hey_bob.isupper() else "Sure."
     else:
-        if re.search(r'\?$', hey_bob):
-            return "Sure."
-        else:
+        if hey_bob.isupper():
+            return "Whoa, chill out!"
+        elif re.search(r'\w+',hey_bob):
             return "Whatever."
-
-
+        else:
+            return "Fine. Be that way!"
--- coba.py
+++ coba.py
@@ -1,10 +1,4 @@
 import re
-isbn1 = "3-598-21508-8"
-isbn2 = "3-598-21507-X"
-isbn3 = "3-598-2X507-9"
 
-# num1 = re.findall(r'[0-9X]', isbn2)
-# print(list(map(int, ['10' if i == 'X' else i for i in num1])))
-
-# print(re.findall(r'[0-9X]', isbn2))
-print(re.search('X', isbn2))
+a = "          "
+print(re.search(r'\w+',a))
